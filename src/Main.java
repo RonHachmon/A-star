64f612f96df3405006054741caba44e8122cbd32
Ron H <hachmonron@gmail.com>
2024-01-27 02:45:27 +0200

    adding maze file parser to logic maze

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,32 @@
 import AStar.AStarPath;
-import AStar.Point;
-
 public class Main {
     public static void main(String[] args) {
-        runPathFind2();
+        runPathFind3();
 
     }
 
     private static void runPathFind() {
-        Point[][] grid=initGrid(4,7);
-        AStarPath path = new AStarPath(grid);
-        blockPath(grid,1,3);
-        blockPath(grid,1,4);
-        blockPath(grid,2,4);
-        path.SetStartPoint(grid[3][1]);
-        path.SetEndPoint(grid[1][6]);
+        FileToMazeConvertor fileToMazeConvertor = new FileToMazeConvertor("src/inputs/4X7_maze");
+        AStarPath path = setPathParameters(fileToMazeConvertor);
         path.getPath();
+
     }
+
+    private static AStarPath setPathParameters(FileToMazeConvertor fileToMazeConvertor) {
+        AStarPath path = new AStarPath(fileToMazeConvertor.getMaze());
+        path.SetStartPoint(fileToMazeConvertor.getStartPoint());
+        path.SetEndPoint(fileToMazeConvertor.getEndPoint());
+        return path;
+    }
+
     private static void runPathFind2() {
-        Point[][] grid=initGrid(6,11);
-        AStarPath path = new AStarPath(grid);
-        blockPath(grid,1,3);
-        blockPath(grid,2,3);
-        blockPath(grid,2,4);
-        blockPath(grid,2,5);
-        blockPath(grid,2,6);
-        blockPath(grid,2,7);
-        path.SetStartPoint(grid[4][7]);
-        path.SetEndPoint(grid[1][4]);
+        FileToMazeConvertor fileToMazeConvertor = new FileToMazeConvertor("src/inputs/6X11_maze");
+        AStarPath path = setPathParameters(fileToMazeConvertor);
         path.getPath();
     }
-
-
-    private static Point[][] initGrid(int width,int length) {
-        Point[][] grid= new Point[width][length];
-
-        for (int i = 0; i <grid.length ; i++) {
-            for (int j = 0; j <grid[i].length ; j++) {
-                grid[i][j] = new Point(i,j);
-            }
-        }
-        return grid;
-    }
-    private static void blockPath(Point[][] grid,int width,int length) {
-
-        grid[width][length].setBlocked(true);
+    private static void runPathFind3() {
+        FileToMazeConvertor fileToMazeConvertor = new FileToMazeConvertor("src/inputs/12X13_maze");
+        AStarPath path = setPathParameters(fileToMazeConvertor);
+        path.getPath();
     }
 }
